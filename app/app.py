58a7eba1e44b22d6d55843b2efeb1d830f25e441
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-03-21 20:44:38 -0300

    Configuración CORS

--- app/app.py
+++ app/app.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
